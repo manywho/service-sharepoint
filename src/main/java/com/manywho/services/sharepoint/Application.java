62985ccd42f3c04257a314dc10c02d791ab86373
jose <jose.collazzi@manywho.com>
2020-04-15 11:36:37 +0100

    FLOW-1359 adding dependencies for logback

--- src/main/java/com/manywho/services/sharepoint/Application.java
+++ src/main/java/com/manywho/services/sharepoint/Application.java
@@ -3,19 +3,24 @@ package com.manywho.services.sharepoint;
 import com.manywho.sdk.services.servers.EmbeddedServer;
 import com.manywho.sdk.services.servers.Servlet3Server;
 import com.manywho.sdk.services.servers.undertow.UndertowServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/")
 public class Application extends Servlet3Server {
+    final static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public Application() {
+        logger.info("Entering application.");
         this.addModule(new ApplicationModule());
         this.setApplication(Application.class);
         this.start();
     }
 
     public static void main(String[] args) throws Exception {
+        logger.info("Entering main.");
         EmbeddedServer server = new UndertowServer();
 
         server.addModule(new ApplicationModule());
